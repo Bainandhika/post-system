303012d92698f2e36fd68e53a69c9d533e3e565e
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-27 22:20:19 +0700

    use custom logger at handlers

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 	"strconv"
 
+	"post-system/app/logging"
 	"post-system/app/services"
 	"post-system/lib/models"
 
@@ -19,13 +22,17 @@ func NewPostsHandler(postsService services.PostsService) *PostsHandler {
 }
 
 func (h *PostsHandler) Insert(c *gin.Context) {
+	urlPath := c.Request.URL.Path
+
 	var payload models.AddPost
 	if err := c.ShouldBindJSON(&payload); err != nil {
+		logging.Error.Println(logDetail(urlPath, payload, err))
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := h.postsService.Insert(payload); err != nil {
+		logging.Error.Println(logDetail(urlPath, payload, err))
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -34,8 +41,11 @@ func (h *PostsHandler) Insert(c *gin.Context) {
 }
 
 func (h *PostsHandler) GetAll(c *gin.Context) {
+	urlPath := c.Request.URL.Path
+
 	posts, err := h.postsService.GetAll()
 	if err != nil {
+		logging.Error.Println(logDetail(urlPath, nil, err))
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -44,19 +54,24 @@ func (h *PostsHandler) GetAll(c *gin.Context) {
 }
 
 func (h *PostsHandler) GetById(c *gin.Context) {
+	urlPath := c.Request.URL.Path
+
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		logging.Error.Println(logDetail(urlPath, id, err))
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	post, err := h.postsService.GetById(id)
 	if err != nil {
+		logging.Error.Println(logDetail(urlPath, id, err))
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	if post == nil {
+		logging.Error.Println(logDetail(urlPath, id, errors.New("post not found")))
 		c.JSON(http.StatusNotFound, gin.H{"error": "post not found"})
 		return
 	}
@@ -65,19 +80,29 @@ func (h *PostsHandler) GetById(c *gin.Context) {
 }
 
 func (h *PostsHandler) Update(c *gin.Context) {
+	urlPath := c.Request.URL.Path
+
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		logging.Error.Println(logDetail(urlPath, id, err))
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	var payload models.UpdatePost
 	if err := c.ShouldBindJSON(&payload); err != nil {
+		logging.Error.Println(logDetail(urlPath, id, err))
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	req := gin.H{
+		"id":   id,
+        "Data": payload,
+	}
+
 	if err := h.postsService.Update(id, payload); err != nil {
+		logging.Error.Println(logDetail(urlPath, req, err))
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -86,16 +111,38 @@ func (h *PostsHandler) Update(c *gin.Context) {
 }
 
 func (h *PostsHandler) Delete(c *gin.Context) {
+	urlPath := c.Request.URL.Path
+
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		logging.Error.Println(logDetail(urlPath, id, err))
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := h.postsService.Delete(id); err != nil {
+		logging.Error.Println(logDetail(urlPath, id, err))
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "post deleted"})
+}
+
+func logDetail(urlPath string, request any, err error) string {
+	detail := struct {
+		URLPath string `json:"url_path"`
+		Request any    `json:"request"`
+		Error   string `json:"error"`
+	}{
+		URLPath: urlPath,
+		Request: request,
+	}
+
+	if err != nil {
+		detail.Error = err.Error()
+	}
+
+	detailBytes, _ := json.Marshal(detail)
+	return string(detailBytes)
 }
