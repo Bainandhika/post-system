2963b5052239c2908e7e26df9a5753199963c76e
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-27 17:17:45 +0700

    create routes

---
+++ app/routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"post-system/app/handlers"
+	"post-system/app/repositories"
+	"post-system/app/services"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetUpRoutes(db *gorm.DB) *gin.Engine {
+	postsRepo := repositories.NewPostsRepo(db)
+	tagsRepo := repositories.NewTagsRepo(db)
+
+	postsService := services.NewPostsService(postsRepo, tagsRepo)
+	postsHandler := handlers.NewPostsHandler(postsService)
+
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.POST("/posts", postsHandler.Insert)
+		api.GET("/posts", postsHandler.GetAll)
+		api.GET("/posts/:id", postsHandler.GetById)
+		api.PUT("/posts/:id", postsHandler.Update)
+		api.DELETE("/posts/:id", postsHandler.Delete)
+	}
+
+	return router
+}
